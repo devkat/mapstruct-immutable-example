eab716b2f53aa0a02a0506f66ac80443a035c42e
andreas.jim-hartmann@id.ethz.ch <andreas.jim-hartmann@id.ethz.ch>
2020-12-15 21:33:48 +0100

    PK class

--- src/main/java/com/github/devkat/persistence/dto/BookToCharacterDto.java
+++ src/main/java/com/github/devkat/persistence/dto/BookToCharacterDto.java
@@ -1,10 +1,32 @@
 package com.github.devkat.persistence.dto;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 
 @Entity
+@IdClass(BookToCharacterDto.BookToCharacterId.class)
 public class BookToCharacterDto {
+
+    public static class BookToCharacterId implements Serializable {
+
+        private final int bookId;
+        private final int characterId;
+
+        public BookToCharacterId(final int bookId, final int characterId) {
+            this.bookId = bookId;
+            this.characterId = characterId;
+        }
+
+        public int getBookId() {
+            return bookId;
+        }
+
+        public int getCharacterId() {
+            return characterId;
+        }
+    }
 
     @Id
     private int bookId;
